711bbd798a832f1c5dbe1898bacd3685e3ae4568
Mitch Torkelson <mitchtorkelson@gmail.com>
2024-05-02 09:37:26 -0400

    dive stats

--- python/divemap.py
+++ python/divemap.py
@@ -10,18 +10,15 @@ total_dive_time_minutes = dive_data['Dive Time (min)'].sum()
 total_dive_time_hours = total_dive_time_minutes // 60
 remaining_minutes = total_dive_time_minutes % 60
 
-# Create a base map, potentially adjusting the initial centering point
-map_dive_locations = folium.Map(location=[0, 0], zoom_start=2, tiles=None)  # Set tiles=None if Esri Ocean Basemap is primary
+# Calculate mean latitude and longitude for map centering
+mean_lat = dive_data['lat'].mean()
+mean_lon = dive_data['lon'].mean()
 
-# Add Esri Ocean Basemap
-esri_ocean = folium.TileLayer(
-    tiles='https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}',
-    attr='Esri',
-    name='Esri Ocean Basemap',
-    overlay=False,
-    control=True
-)
-esri_ocean.add_to(map_dive_locations)
+# Create a base map, centering on the mean location
+map_dive_locations = folium.Map(location=[mean_lat, mean_lon], zoom_start=4)
+
+# Add the Stamen Terrain tiles with proper attribution
+folium.TileLayer('Stamen Watercolor', attr='Map data © OpenStreetMap contributors, CC-BY-SA, Imagery © Stamen Design').add_to(map_dive_locations)
 
 # HTML to display stats
 html = f"""
@@ -34,9 +31,8 @@ html = f"""
 </div>
 """
 
-# Add this HTML to the map using folium
+# Add this HTML to the map
 map_dive_locations.get_root().html.add_child(folium.Element(html))
-
 
 # Save the map
 map_dive_locations.save('/Users/mitchtork/website/assets/files/DiveLog.html')
