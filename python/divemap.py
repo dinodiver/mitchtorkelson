06b0e145f67411e4628246c9fb4e4397f4e09e90
Mitch Torkelson <mitchtorkelson@gmail.com>
2024-05-02 09:06:16 -0400

    dive stats addition

--- python/divemap.py
+++ python/divemap.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import folium
 from folium.plugins import MarkerCluster
-from folium import IFrame
 
 # Load your CSV file containing dive data
 dive_data = pd.read_csv('/Users/mitchtork/website/assets/files/DiveLog.csv')
@@ -15,18 +14,17 @@ remaining_minutes = total_dive_time_minutes % 60
 # Aggregate the data by location and count the number of dives
 location_counts = dive_data.groupby(['Location', 'lat', 'lon']).size().reset_index(name='counts')
 
+# Determine the maximum number of dives at any single location for color scaling
+max_dives = location_counts['counts'].max()
+
 # Create a base map
 map_dive_locations = folium.Map(location=[0, 0], zoom_start=2)
 
 # Marker cluster
 marker_cluster = MarkerCluster().add_to(map_dive_locations)
 
-# Maximum number of dives for scaling
-max_dives = location_counts['counts'].max()
-
 # Loop through the aggregated data and add each location as a marker on the map
 for idx, row in location_counts.iterrows():
-    # Define color based on the number of dives
     color = f"#{int((1 - row['counts'] / max_dives) * 255):02x}0000ff"
     folium.CircleMarker(
         location=[row['lat'], row['lon']],
@@ -41,24 +39,22 @@ for idx, row in location_counts.iterrows():
         fill_opacity=row['counts'] / max_dives
     ).add_to(marker_cluster)
 
-# Prepare the HTML for the dive stats
+# Prepare the HTML for the dive stats, styled as a ticker
 html = f"""
-<div style="position: fixed; 
-            bottom: 50px; left: 50px; width: 300px; height: 100px; 
-            background: white; border-radius: 8px; padding: 10px; 
-            box-shadow: 3px 3px 3px rgba(0,0,0,0.2);">
-    <h4>Dive Stats</h4>
-    <ul>
-        <li>Total Dives: {total_dive_count}</li>
-        <li>Total Time Underwater: {total_dive_time_hours} hours and {remaining_minutes} minutes</li>
-    </ul>
+<div style="background-color: rgba(0,0,0,0.8); color: white; font-size:16px; padding:10px; border-radius: 8px; border: 2px solid #0078A8; position: absolute; bottom: 20px; left: 20px; z-index:1000;">
+    <strong>Dive Stats:</strong> {total_dive_count} Dives | {total_dive_time_hours}h {remaining_minutes}m Underwater
 </div>
 """
 
-# Create an IFrame and add it to the map
-iframe = IFrame(html=html, width=350+20, height=130+20)
-popup = folium.Popup(iframe, max_width=2650)
-folium.Marker([0, 0], icon=folium.Icon(icon="info-sign")).add_child(popup).add_to(map_dive_locations)
+# Create a DivIcon that will use the HTML
+div_icon = folium.DivIcon(
+    icon_size=(None, None),
+    icon_anchor=(0, 0),
+    html=html,
+)
+
+# Add this div icon to the map
+folium.Marker(location=[0, 0], icon=div_icon, opacity=0).add_to(map_dive_locations)
 
 # Save the map to an HTML file
 map_dive_locations.save('/Users/mitchtork/website/assets/files/DiveLog.html')
